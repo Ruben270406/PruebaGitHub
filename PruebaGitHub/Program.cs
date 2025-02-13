8d69510b14b0522c9deaa2d8f049d825e50ac3b1
Ruben <rvidabravo@gmail.com>
2025-02-13 14:19:23 +0100

    Se ha agregado  la estructura del programa

--- PruebaGitHub/Program.cs
+++ PruebaGitHub/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // VARIABLES 
+
+            // ENTRADA
+
+            // PROCESO
+
+            // SALIDA
         }
     }
+
 }
