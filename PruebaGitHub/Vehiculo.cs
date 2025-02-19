7a35d1094eb9b1bdeba414b28e4c1be521cdf40c
Ruben <rvidabravo@gmail.com>
2025-02-19 09:07:41 +0100

    Establecimiento de la estructura del Modelo de Datos (Clase Instanciable)

--- PruebaGitHub/Vehiculo.cs
+++ PruebaGitHub/Vehiculo.cs
@@ -11,5 +11,26 @@ namespace PruebaGitHub
     /// </summary>
     public class Vehiculo
     {
+        #region CONSTANTES - PARÁMETROS DE CONFIGURACIÓN DE LA CLASE
+        #endregion
+
+        #region MIEMBROS | CAMPOS | ATRIBUTOS PRIVADOS
+        #endregion
+
+        #region CONSTRUCTORES 
+        #endregion
+
+        // Mecanismo de acceso a la informaciñon que mantiene la clase (datos)
+        // Proporciona dos descriptores de acceso (get y set)
+        // Su misión es proteger los datos que entran a la clase (set)
+        // o se transfieren fuera de la clase (get)
+        #region PROPIEDADES
+        #endregion
+
+        #region MÉTODOS PRIVADOS - INTERNOS DE LA CLASE
+        #endregion
+
+        #region MÉTODOS PÚBLICOS DE LA CLASE - ACCESIBLES DESDE FUERA DE LA CLASE
+        #endregion
     }
 }
