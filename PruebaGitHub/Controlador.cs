710742f64450965e4f6a63690bbe55d0d6aa5ad7
Ruben <rvidabravo@gmail.com>
2025-02-19 08:45:40 +0100

    Estructura Organizativa del proyecto: Clases Instanciables  y Estáticas

---
+++ PruebaGitHub/Controlador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGitHub
+{
+    /// <summary>
+    /// Clase Estática que sirve de intermediaria entre el Modelo y las Vistas de la aplicación
+    /// </summary>
+    public static class Controlador
+    {
+    }
+}
